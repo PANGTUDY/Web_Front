d31794705dd4799775a1bdee481f3a49e85bdb22
slolee <slolee@naver.com>
2021-12-12 15:17:27 +0900

    ch4njun-001 Calendar 페이지 나갈때 SSE 연결해제

--- src/views/conference/Calendar.vue
+++ src/views/conference/Calendar.vue
@@ -19,19 +19,46 @@
             
                 <el-card shadow="never">
                     <el-row v-for="item in this.calendar[this.year + '-' + this.month + '-' + this.day]" :key="item.id">
-                        <el-col :span="24"><div class="grid-content bg-purple-light">{{ item.comment }}</div></el-col>
+                        <el-col :span="24">
+                            <div class="grid-content bg-purple-light">{{ item.comment }}</div>
+                        </el-col>
                     </el-row>
                 </el-card>
             </div>
         </div>
+        <modal :show.sync="change"
+                gradient="danger"
+                modal-classes="modal-danger modal-dialog-centered">
+            <h6 slot="header" class="modal-title" id="modal-title-notification">Warning</h6>
+
+            <div class="py-3 text-center">
+                <i class="ni ni-bell-55 ni-3x"></i>
+                <h4 class="heading mt-4">현재 날짜의 스케줄이 변경되었습니다.</h4>
+            </div>
+
+            <template slot="footer">
+                <base-button type="link"
+                                text-color="white"
+                                class="ml-auto"
+                                @click="change = false">
+                    Ok, Got it
+                </base-button>
+            </template>
+        </modal>
     </div>
 </template>
 
 <script>
+    import Modal from "@/components/Modal.vue";
     export default {
+        components: {
+            Modal
+        },
         data() {
             return {
                 select_date: new Date(),
+                change: false,
+                sse_source: null
             }
         },
         computed: {
@@ -48,22 +75,23 @@
                 return this.$store.state.calendar;
             }
         },
-        methods: {
-            test(data, date) {
-                alert(data + "\n" + date);
-                console.log(this.calendar);
-                console.log(this.selectDate);
-            }
-        },
         mounted() {
             this.$store.dispatch("load_calendar", this.year);
-            var source = new EventSource("http://localhost:10831/calendar/schedules/sse");
-            source.onmessage = (event) => { 
+            // TODO : EventSource 주소 상수화 필요
+            this.sse_source = new EventSource("http://localhost:10831/calendar/schedules/sse");
+            this.sse_source.onmessage = (event) => { 
                 var event_data = JSON.parse(event.data);
                 if (event_data.schedule.year === this.year) {
+                    if (event_data.schedule.month === this.month && event_data.schedule.day === this.day) {
+                        this.change = true;
+                    }
                     this.$store.dispatch("call_calendar_event", event_data);
                 }
             }
+        },
+        beforeDestroy() {
+            this.sse_source.close();
+            this.sse_source = null;
         }
     }
 </script>
