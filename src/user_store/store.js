a1f9479522eb450645efbd079e26d3556d38bdf6
slolee <slolee@naver.com>
2022-01-11 23:27:41 +0900

    ch4njun-001 Calendar Schedule 추가/수정시 정렬되도록 구성

--- src/user_store/store.js
+++ src/user_store/store.js
@@ -37,20 +37,21 @@ export default new Vuex.Store({
                 else
                     state.calendar[date] = [element];
             });
-            console.log(state.calendar);
             state.current_year = payload.year;
         },
         CREATE_SCHEDULE(state, payload) {
             var date = payload.schedule.year + '-' + payload.schedule.month + '-' + payload.schedule.day;
-            if (date in state.calendar) 
-                state.calendar[date].push(payload.schedule);
-            else
+            if (date in state.calendar) {
+                for (var i = 0; i < state.calendar[date].length; ++i) {
+                    if (state.calendar[date][i]['startTime'][0] <= payload.schedule.startTime[0] &&
+                            state.calendar[date][i]['startTime'][1] < payload.schedule.startTime[1]) {
+                        state.calendar[date].splice(i + 1, 0, payload.schedule);
+                        break;
+                    }
+                }
+            } else {
                 state.calendar[date] = [payload.schedule];
-        },
-        UPDATE_SCHEDULE(state, payload) {
-           var date = payload.schedule.year + '-' + payload.schedule.month + '-' + payload.schedule.day;
-           var update_id = state.calendar[date].findIndex(schedule => schedule.id === payload.schedule.id);
-           state.calendar[date][update_id] = payload.schedule;
+            }
         },
         DELETE_SCHEDULE(state, payload) {
             for (const date in state.calendar) {
@@ -93,7 +94,8 @@ export default new Vuex.Store({
                     commit('CREATE_SCHEDULE', { schedule: event_data.schedule });
                     break;
                 case 'MODIFY':
-                    commit('UPDATE_SCHEDULE', { schedule: event_data.schedule });
+                    commit('DELETE_SCHEDULE', { id: event_data.schedule.id });
+                    commit('CREATE_SCHEDULE', { schedule: event_data.schedule });
                     break;
                 case 'DELETE':
                     commit('DELETE_SCHEDULE', { id: event_data.schedule.id });
