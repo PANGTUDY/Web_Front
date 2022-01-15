dea127998d0d7b71bc7818935e9faadad4396aff
heywon0909 <hw7319@naver.com>
2022-01-15 10:38:30 +0900

    .

--- src/views/components/board/List.vue
+++ src/views/components/board/List.vue
@@ -1,67 +1,62 @@
 <template>
-    <v-app>
-        <div class="container pt-lg-md" style="min-height: 800px">
-            <div class="row justify-content-center">
-                <div class="col-lg-2">
-                    <v-select
-                        :items="selectItems"
-                        label="전체"
-                        single-line
-                    ></v-select>
-                </div>
-                <div class="col-lg-8">
-                    <v-text-field
-                        v-model="keyword"
-                        :append-icon="'fa-search'"
-                        placeholder="키워드를 입력하세요"
-                        hide-details="auto"
-                        type="text"
-                        @click:append="searchIcon"
-                        v-on:keyup.enter="searchIcon"
-                    >
-                        <v-icon>fas fa-search</v-icon>
-                    </v-text-field>
-                </div>
+    <div class="container pt-lg-md" style="min-height: 800px">
+        <div class="row justify-content-center">
+            <div class="col-lg-2">
+                <v-select
+                    :items="selectItems"
+                    label="전체"
+                    single-line
+                ></v-select>
             </div>
-            <div class="row justify-content-center">
-                <div class="col-lg-2">
-                    <v-sheet rounded="lg">
-                        <v-list color="transparent">
-                            <v-subheader>CATEGORY</v-subheader>
-                            <v-list-item-group
-                                v-model="selectedItem"
-                                color="primary"
-                            >
-                                <v-list-item
-                                    v-for="(item, idx) in category_list"
-                                    :key="idx"
-                                >
-                                    <v-list-item-content>
-                                        <v-list-item-title 
-                                            v-text="item.categoryName"
-                                            @click="categoryChange(idx)"
-                                        >
-                                        </v-list-item-title>
-                                    </v-list-item-content>
-                                </v-list-item>
-                            </v-list-item-group>
-                        </v-list>
-                    </v-sheet>
-                </div>
-                <div class="col-lg-8">
-                    <div class="row justify-content-right">
-                        <div class="col-lg-10">
-                            <h4>{{category}}</h4>
-                        </div>
-                        <div class="col-lg-2 text-right">
-                            <v-btn
+            <div class="col-lg-8">
+                <v-text-field
+                    v-model="keyword"
+                    :append-icon="'fa-search'"
+                    placeholder="키워드를 입력하세요"
+                    hide-details="auto"
+                    type="text"
+                    @click:append="searchIcon"
+                    v-on:keyup.enter="searchIcon"
+                >
+                    <v-icon>fas fa-search</v-icon>
+                </v-text-field>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-2">
+                <v-sheet rounded="lg">
+                    <v-list color="transparent">
+                        <v-subheader>CATEGORY</v-subheader>
+                        <v-list-item-group
+                            v-model="selectedItem"
                             color="primary"
-                            elevation="2"
-                            outlined
-                            v-on:click="newPost"
-                            >글쓰기</v-btn>
-                        </div>
-                   </div>
+                        >
+                            <v-list-item
+                                v-for="(item, i) in categoryItems"
+                                :key="i"
+                            >
+                                <v-list-item-content>
+                                    <v-list-item-title v-text="item.text"></v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list>
+                </v-sheet>
+            </div>
+            <div class="col-lg-8">
+                <div class="row justify-content-right">
+                    <div class="col-lg-10">
+                        <h4>전체 글</h4>
+                    </div>
+                    <div class="col-lg-2 text-right">
+                        <v-btn
+                        color="primary"
+                        elevation="2"
+                        outlined
+                        v-on:click="newPost"
+                        >글쓰기</v-btn>
+                    </div>
+                </div>
                 <div class="row">
                     <div class="col-lg-12">
                         <table class="tbList">
@@ -79,21 +74,20 @@
                                 <td colspan="4">데이터가 없습니다.</td>
                             </tr>
                         </table>
-                        </div>
                     </div>
-                    <div>
-                        <div class="text-center">
-                            <v-pagination
-                            v-model="page"
-                            :length="6"
-                            color="#0D47A1"
-                            ></v-pagination>
-                        </div>
-                    </div>                 
                 </div>
+                <div>
+                    <div class="text-center">
+                        <v-pagination
+                        v-model="page"
+                        :length="6"
+                        color="#0D47A1"
+                        ></v-pagination>
+                    </div>
+                </div>                 
             </div>
         </div>
-    </v-app>
+    </div>
 </template>
 
 <script>
@@ -102,17 +96,21 @@ import * as Api from '@/api/board.js';
 export default {
     data: () => ({
         keyword: '',
-        category_list: [],
-        category: '',
-        posts: [],
+        list: [],
+        // list: [
+        //     { id: '1', subject: '제목1', name: '김민주', regdate: '2021-10-10'},
+        //     { id: '2', subject: '제목2', name: '김민주', regdate: '2021-10-10'},
+        //     { id: '3', subject: '제목3', name: '김민주', regdate: '2021-10-10'},
+        //     { id: '4', subject: '제목4', name: '김민주', regdate: '2021-10-10'},
+        // ],
         selectedItem: 0,
         hint: '키워드를 입력하세요',
         select: { text:'제목', value: 0 },
         selectItems: [
-            { text:'제목', value: 0 },
-            { text:'제목+내용', value: 1 },
-            { text:'해시태그', value: 2 },
-            { text:'작성자', value: 3 },
+            '제목',
+            '제목+내용',
+            '해시태그',
+            '작성자',
         ],
         categoryItems: [
             { text: '전체 글' },
@@ -125,12 +123,7 @@ export default {
     methods: {
         // Search the keyword
         searchIcon() {
-            if(this.keyword.length > 0) {
-                alert(this.keyword);
-            }
-            else {
-                alert("검색된 키워드가 없습니다!");
-            }
+            alert(this.keyword)
         },
 
         // create a new post
@@ -143,37 +136,13 @@ export default {
             console.log(id);
 			this.$router.push({path:'./view/' + id});
         },
-
-        // change the category
-        categoryChange(idx) {
-            console.log(this.category_list[idx]);
-            this.category = this.category_list[idx].categoryName;
-        },
     },
     mounted() {
-        // 전체 글 목록 불러오는 Api
         Api.get_post_list().then(data => {
             console.log(data);
-            this.posts = data;
         })
         .catch(error => {
-            console.log("error occured!: ", error);
-        });
-
-        // 카테고리 전체 목록 불러오는 Api
-        Api.get_category_list().then(data => {
-            console.log(data.data);
-            this.category_list = data.data;
-            this.category_list.push({categoryId: 0, categoryName: "전체"});
-            console.log('category: ', this.category_list);
-
-            this.category_list.sort(function(a, b) {
-                return a.categoryId - b.categoryId;
-            });
-            this.category = this.category_list[0].categoryName;
-        })
-        .catch(error => {
-            console.log("error occured!: ", error);
+            console.log(error);
         });
     },
 }
@@ -190,5 +159,4 @@ export default {
 	.first, .prev, .next, .last{border:1px solid #666; margin:0 5px;}
 	.pagination span{display:inline-block; padding:0 5px; color:#333;}
 	.pagination a{text-decoration:none; display:inline-blcok; padding:0 5px; color:#666;}
-    .hash{font-size: 10px;}
 </style>
