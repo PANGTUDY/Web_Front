ae8bc7913b2982fe4945d2ac3f1bb40fa54cf3ac
hiMinju <minju980508@gmail.com>
2022-02-01 03:51:58 +0900

    Change the related with category and layout in board

--- src/api/board.js
+++ src/api/board.js
@@ -4,6 +4,19 @@ export async function get_post_list() {
     return boardInstance.get("/board/posts");
 }
 
+export async function get_category_post_list() {
+    //TODO 특정 카테고리 게시물
+    return boardInstance.get("/board/posts");
+}
+
 export async function get_category_list() {
     return boardInstance.get("/board/categories");
+}
+
+export async function create_post(post) {
+    return instance.post("/board/posts", post, {
+        headers: {
+            'Content-Type': `application/json`,
+        },
+    });
 }
