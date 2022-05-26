1d04b292d6ada8eadd8a1714cafd9a8828b3a040
heywon0909 <hw7319@naver.com>
2022-05-26 20:10:33 +0900

    beforeEach 수정중

--- src/router/beforeEach.js
+++ src/router/beforeEach.js
@@ -1,13 +1,18 @@
 import store from '../store/index.js'
 export default function (to, from, next) {
+    console.log('to', to);
+    console.log('from', from);
     if (to.path !== '/login') {
         if (to.path === '/' ) {
             next();
             
         } else {
             if (store.state.isLogin === false) {
-                store.state.popupSetting = true;
-                next('/login');
+                if (to.path === '/register') { next(); }
+                else {
+                    store.state.popupSetting = true;
+                    next('/login');
+                }
             } else {
                 next();
             }
